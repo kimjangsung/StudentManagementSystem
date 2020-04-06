f7baba285ae592a39b7409088fbe0ee299e6b8ba
user <user@user-PC>
2020-04-07 04:26:24 +0900

    create student and student manager classes

---
+++ src/Student.java
@@ -0,0 +1,26 @@
+
+public class Student {
+
+	String name;
+	int id;
+	String email;
+	String phone;
+
+	public Student() {
+	}
+	public Student(String name, int id) {
+		this.name = name;
+		this.id = id;
+	}
+
+	public Student(String name, int id, String email, String phone) {
+		this.name = name;
+		this.id = id;
+		this.email = email;
+		this.phone = phone;
+
+	}
+	public void printInof() {
+		System.out.println("name : " + name + "id : " + id + "email : " + email + "phone : " + phone);
+	}
+}
